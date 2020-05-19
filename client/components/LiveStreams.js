00f45bfbf32e86e7dc247a7dc853d8d2543dce76
saman222 <samansamani2@yahoo.com>
2020-05-19 17:07:17 +0430

    Update LiveStreams.js

--- client/components/LiveStreams.js
+++ client/components/LiveStreams.js
@@ -19,7 +19,8 @@ export default class Navbar extends React.Component {
     }
 
     getLiveStreams() {
-        axios.get('http://127.0.0.1:' + config.rtmp_server.http.port + '/api/streams')
+//        axios.get('http://127.0.0.1:' + config.rtmp_server.http.port + '/api/streams')
+        axios.get(window.location.protocol + "//" + window.location.hostname + ":" + config.rtmp_server.http.port + '/api/streams')
             .then(res => {
                 let streams = res.data;
                 if (typeof (streams['live'] !== 'undefined')) {
@@ -37,6 +38,7 @@ export default class Navbar extends React.Component {
             this.setState({
                 live_streams: res.data
             }, () => {
+		console.log(this);
                 console.log(this.state);
             });
         });
